822c42e53b504df160f7ae40e89ab38e88db274a
wanjiku <annritawanjiku77@gmail.com>
2023-09-07 23:05:04 +0300

    dynamic day of the week

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ document.addEventListener('DOMContentLoaded', function (){
         return todaysDate.toISOString().substr(11,8)
     }
 
+    // get current day
+    function getCurrentDay(){
+        const todaysDate = new Date ()
+        const dayIndex = todaysDate.getUTCDay()
+        const daysOfTheWeek =  ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        return  daysOfTheWeek[dayIndex]
+
+    }
+
     // Make content dynamic
     document.querySelector('[data-testid="currentUTCTime"]').textContent = getCurrentTimeInUTC()
+    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = getCurrentDay()
 })
